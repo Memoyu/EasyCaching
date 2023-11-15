87fbf3ff69f1706b2fb2529e72624e26ca5efef1
Memoyu <mmy6076@outlook.com>
2023-11-15 23:53:06 +0800

    feat: add test

---
+++ sample/EasyCaching.Demo.Memory/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EasyCaching.Core;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+Console.WriteLine("Start Cache Test");
+
+IServiceCollection services = new ServiceCollection();
+
+services.AddMemoryCache();
+services.AddEasyCaching(option =>
+{
+    option.UseInMemory("m1")
+    .WithJson("json");
+});
+
+var provider = services.BuildServiceProvider();
+var ec = provider.GetRequiredService<IEasyCachingProvider>();
+var mc = provider.GetRequiredService<IMemoryCache>();
+
+var count = 10000;
+var sw = new Stopwatch();
+sw.Start();
+for (int i = 0; i < count; i++)
+{
+    var key = "ec-memory-key-" + i ;
+    ec.Set(key, i, TimeSpan.FromMinutes(10));
+}
+sw.Stop();
+Console.WriteLine($"ec-Set  count:{count},time:{sw.Elapsed.TotalMilliseconds}ms");
+
+sw.Restart();
+for (int i = 0; i < count; i++)
+{
+    var key = "mc-memory-key-" + i;
+    mc.Set(key, i, TimeSpan.FromMinutes(10));
+}
+sw.Stop();
+Console.WriteLine($"mc-Set  count:{count},time:{sw.Elapsed.TotalMilliseconds}ms");
+
+
+Console.WriteLine("End");
